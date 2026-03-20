516b265f910d6f14ac6bdc30933e8a39cc23c5e2
“Nihal <nihalj09@gmail.com>
2026-03-20 17:18:25 -0400

    fix errors

--- src/fetch_stocks.py
+++ src/fetch_stocks.py
@@ -4,7 +4,8 @@ import pandas as pd
 #Fetches the list of S&P 500 ticker symbols from Wikipedia.
 def get_sp500_tickers():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    data = pd.read_html(url)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    data = pd.read_html(url, storage_options=headers)
     stocks = data[0]['Symbol']
     stocks = stocks.str.replace('.', '-', regex=False)
     return stocks.tolist()
@@ -12,8 +13,9 @@ def get_sp500_tickers():
 #Fetched historical OHLCV price data for a single stock ticker
 def fetch_stock_data(ticker, start_date, end_date):
     stock_data = yf.download(ticker, start=start_date, end=end_date, auto_adjust=True)
+    stock_data.columns = [col[0] if isinstance(col, tuple) else col for col in stock_data.columns]
     stock_data = stock_data.reset_index()
-    ticker = stock_data['Ticker']
+    stock_data['Ticker'] = ticker
     stock_data = stock_data.dropna()
     return stock_data
 
@@ -21,7 +23,8 @@ def fetch_stock_data(ticker, start_date, end_date):
 def fetch_multiple_stocks(tickers, start_date, end_date):
     all_stocks = []
     for ticker in tickers:
-        df = fetch_stock_data(tickers, start_date, end_date)
+        df = fetch_stock_data(ticker, start_date, end_date)
         all_stocks.append(df)
     combined = pd.concat(all_stocks, ignore_index=True)
     return combined    
+
