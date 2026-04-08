74717ee2b3fc9adfd3a83cad72723b3a1754151a
“Nihal <nihalj09@gmail.com>
2026-04-07 21:23:46 -0400

    add imports and load model function

---
+++ src/predict.py
@@ -0,0 +1,17 @@
+import numpy as np
+import joblib
+import os
+
+from fetch_stocks import fetch_stock_data
+from fetch_news import fetch_news_for_ticker
+from features import prepare_features
+from sentiment import process_news_sentiment
+
+def load_model(model_path='models/random_forest.pkl', scaler_path='models/scaler.pkl'):
+    if not os.path.exists(model_path) or not os.path.exists(scaler_path):
+        raise FileNotFoundError("Model not found. Run train.py first.")
+    
+    forest = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+    
+    return forest, scaler
